19087f18fb7a026e70f55313d7095fa868627db8
Ali pakdaman <alipakdaman68@gmail.com>
2022-06-17 11:20:47 +0430

    add randomness to aes

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
+	"io"
 	"log"
 )
 
@@ -13,14 +15,14 @@ func main() {
 	msg := " this is an email from ali to behnaz, thank you toi share me information , and hel me!"
 	password := "ilovedogs"
 	key, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	rslt, err := enDecode(key[0:16], msg)
+	rslt, iv, err := encod(key[0:32], msg)
 
 	if err != nil {
-		log.Fatalln("errros, ", err)
+		log.Fatalln("errros, ", err, "iv= ", iv)
 	}
 	fmt.Println(string(rslt))
 
-	rslt2, err := enDecode(key[0:16], string(rslt))
+	rslt2, err := decod(key[0:32], iv, string(rslt))
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -36,7 +38,35 @@ func main() {
 
 }
 
-func enDecode(key []byte, input string) ([]byte, error) {
+func encod(key []byte, input string) ([]byte, []byte, error) {
+	b, errr := aes.NewCipher(key)
+	if errr != nil {
+		return nil, nil, fmt.Errorf("error in newcier, %w", errr)
+	}
+	//initialig vector
+	//iv := []byte(string(aes.BlockSize))
+
+	//initializingvector
+	iv := make([]byte, aes.BlockSize)
+	//put random numbers in iv
+	_, err := io.ReadFull(rand.Reader, iv)
+	buff := &bytes.Buffer{}
+	s := cipher.NewCTR(b, iv)
+	sw := cipher.StreamWriter{
+		S:   s,
+		W:   buff,
+		Err: nil,
+	}
+	_, err = sw.Write([]byte(input))
+	if errr != nil {
+		return nil, iv, fmt.Errorf("eror in write cipher , %w", err)
+	}
+
+	return buff.Bytes(), iv, nil
+
+}
+
+func decod(key []byte, ivin []byte, input string) ([]byte, error) {
 	b, errr := aes.NewCipher(key)
 	if errr != nil {
 		return nil, fmt.Errorf("error in newcier, %w", errr)
@@ -45,8 +75,9 @@ func enDecode(key []byte, input string) ([]byte, error) {
 	//iv := []byte(string(aes.BlockSize))
 
 	//initializingvector
-	iv := make([]byte, aes.BlockSize)
+	iv := ivin
 	//put random numbers in iv
+	//_, err := io.ReadFull(rand.Reader, iv)
 	buff := &bytes.Buffer{}
 	s := cipher.NewCTR(b, iv)
 	sw := cipher.StreamWriter{

905e20816a699dcc3675b559954188f8692133d8
Ali pakdaman <alipakdaman68@gmail.com>
2022-06-17 11:10:46 +0430

    aes ecdecode example

--- main.go
+++ main.go
@@ -1,44 +1,64 @@
 package main
 
 import (
-	"encoding/base64"
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"log"
 )
 
 func main() {
 	msg := " this is an email from ali to behnaz, thank you toi share me information , and hel me!"
-	encMsg := encodebase64std(msg)
-	fmt.Printf("my msg: %s, this is encoded msg: %s", msg, encMsg)
-	fmt.Printf(" this is encoded url msg: %s", encodebase64url(msg))
-	decoMsg, err := decodebase64std([]byte(encMsg))
+	password := "ilovedogs"
+	key, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	rslt, err := enDecode(key[0:16], msg)
+
 	if err != nil {
-		fmt.Printf("there was an error decoding MSG!")
+		log.Fatalln("errros, ", err)
 	}
-	fmt.Printf("decoded msg : %s", decoMsg)
+	fmt.Println(string(rslt))
 
-}
-
-func encodebase64std(msg string) string {
-	return base64.StdEncoding.EncodeToString([]byte(msg))
-
-}
-func encodebase64url(msg string) string {
-	return base64.URLEncoding.EncodeToString([]byte(msg))
-}
-func decodebase64std(encMsg []byte) (string, error) {
-	dsc := make([]byte, len(encMsg))
-	_, err := base64.StdEncoding.Decode(dsc, encMsg)
+	rslt2, err := enDecode(key[0:16], string(rslt))
 	if err != nil {
-		return "", fmt.Errorf("error in decodebase while decoding, %w", err)
+		log.Fatalln(err)
 	}
-	return string(dsc), nil
+	fmt.Println(string(rslt2))
+	//encMsg := encodebase64std(msg)
+	//fmt.Printf("my msg: %s, this is encoded msg: %s", msg, encMsg)
+	//fmt.Printf(" this is encoded url msg: %s", encodebase64url(msg))
+	//decoMsg, err := decodebase64std([]byte(encMsg))
+	//if err != nil {
+	//	fmt.Printf("there was an error decoding MSG!")
+	//}
+	//fmt.Printf("decoded msg : %s", decoMsg)
 
 }
-func decodeurl(enc string) (string, error) {
-	b, err := base64.URLEncoding.DecodeString(enc)
-	if err != nil {
-		return "", fmt.Errorf("error %w", err)
+
+func enDecode(key []byte, input string) ([]byte, error) {
+	b, errr := aes.NewCipher(key)
+	if errr != nil {
+		return nil, fmt.Errorf("error in newcier, %w", errr)
 	}
-	return string(b), nil
+	//initialig vector
+	//iv := []byte(string(aes.BlockSize))
+
+	//initializingvector
+	iv := make([]byte, aes.BlockSize)
+	//put random numbers in iv
+	buff := &bytes.Buffer{}
+	s := cipher.NewCTR(b, iv)
+	sw := cipher.StreamWriter{
+		S:   s,
+		W:   buff,
+		Err: nil,
+	}
+	_, err := sw.Write([]byte(input))
+	if errr != nil {
+		return nil, fmt.Errorf("eror in write cipher , %w", err)
+	}
+
+	return buff.Bytes(), nil
 
 }

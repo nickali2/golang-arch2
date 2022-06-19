dc7eb84aa70398f32269edc52307281da25ff87d
Ali pakdaman <alipakdaman68@gmail.com>
2022-06-19 11:40:34 +0430

    ninja02 part2 add login page

--- main.go
+++ main.go
@@ -7,18 +7,21 @@ import (
 	"net/url"
 )
 
+var db = map[string][]byte{}
+
 func main() {
 
 	http.HandleFunc("/", defaul)
 	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 
 	http.ListenAndServe("localhost:8080", nil)
 }
 
 func defaul(w http.ResponseWriter, r *http.Request) {
 	errmsg := r.FormValue("errormsg")
-	username := r.FormValue("erruse")
-	password := r.FormValue("errorpass")
+	//username := r.FormValue("errormsg")
+	//password := r.FormValue("errormsg")
 
 	fmt.Fprintf(w, `<!DOCTYPE html>
 <html lang="en">
@@ -27,34 +30,42 @@ func defaul(w http.ResponseWriter, r *http.Request) {
     <title>hello</title>
 </head>
 <body>
-<p>%s</p>
-<p>%s</p>
-<p>%s</p>
+<p>error: %s</p>
+<h1 >register</h1>
 <form action="/register" method="post">
     <input name="username" type="text" >
     <input name="password" type="password">
     <input name="register" type="submit">
 </form>
+<h1>login</h1>
+<form action="/login" method="get" >
+    <input name="username" type="text" >
+    <input name="password" type="password">
+    <input name="login" type="submit">
+
+</form>
 </body>
-</html>`, errmsg, username, password)
+</html>`, errmsg)
 	//io.WriteString(w, html)
 }
 
-type userdata struct {
-	User map[string][]byte `json:"user"`
-}
+//type userdata struct {
+//	User map[string][]byte `json:"user"`
+//}
+
+//var db = userdata{}
 
 func register(w http.ResponseWriter, r *http.Request) {
-	db := userdata{}
+
 	if r.Method != http.MethodPost {
-		erruse := url.QueryEscape("methos was not post")
-		http.Redirect(w, r, "/?errormsg="+erruse, http.StatusSeeOther)
+		errmsg := url.QueryEscape("methos was not post")
+		http.Redirect(w, r, "/?errormsg="+errmsg, http.StatusSeeOther)
 		return
 	}
 	username := r.FormValue("username")
 	if username == "" {
-		errmsg := url.QueryEscape("username cannot be empty!")
-		http.Redirect(w, r, "/?errormsg="+errmsg, http.StatusSeeOther)
+		erruse := url.QueryEscape("username cannot be empty!")
+		http.Redirect(w, r, "/?errormsg="+erruse, http.StatusSeeOther)
 		return
 	}
 	password := r.FormValue("password")
@@ -64,13 +75,47 @@ func register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	hashpass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MaxCost)
+	hashpass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		http.Error(w, "Ooops!, try again!", http.StatusInternalServerError)
 		return
 	}
 
-	db.User[username] = hashpass
+	db[username] = hashpass
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		errmsg := url.QueryEscape("methos was not get")
+		http.Redirect(w, r, "/?errormsg="+errmsg, http.StatusSeeOther)
+		return
+	}
+	username := r.FormValue("username")
+	if username == "" {
+		erruse := url.QueryEscape("username cannot be empty!")
+		http.Redirect(w, r, "/?errormsg="+erruse, http.StatusSeeOther)
+		return
+	}
+	password := r.FormValue("password")
+	if password == "" {
+		errpass := url.QueryEscape("password cannot be empty!")
+		http.Redirect(w, r, "/?errormsg="+errpass, http.StatusSeeOther)
+		return
+	}
+	if _, ok := db[username]; !ok {
+		errmsg := url.QueryEscape("username or password is not correct")
+		http.Redirect(w, r, "/?errormsg="+errmsg, http.StatusSeeOther)
+		return
+	}
+	err := bcrypt.CompareHashAndPassword(db[username], []byte(password))
+	if err != nil {
+		errmsg := url.QueryEscape("username or password is not correct")
+		http.Redirect(w, r, "/?errormsg="+errmsg, http.StatusSeeOther)
+		return
+	}
+	errmsg := url.QueryEscape("Logged IN! " + username)
+	http.Redirect(w, r, "/?errormsg="+errmsg, http.StatusSeeOther)
 }
